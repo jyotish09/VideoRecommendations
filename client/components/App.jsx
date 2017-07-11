10f9701d1bb6fb0ab7569ca95f7e3057dbf65275
jyotish09 <dontsentmemails@yahoo.com>
2017-07-11 10:59:29 +0530

    Firebase addition

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import {Button, ProgressBar} from 'react-bootstrap';
 import 'bootstrap/less/bootstrap.less';
-
+import fire from './firebaseKeys.js';
 export default class App extends React.Component {
     render() {
+        console.log(fire);
         return (
-            <div style={{
+            <div className="container" style={{
                 textAlign: 'center'
             }}>
                 <h1>Hello World</h1>
-                <Button bsStyle='success' bsSize='large' style={{marginBottom:'1%'}}>Get started today</Button>
+                <Button bsStyle='success' bsSize='large' style={{
+                    marginBottom: '1%'
+                }}>Get started today</Button>
 
-                <ProgressBar bsStyle="success" now={40}/>
+                <ProgressBar active bsStyle="success" now={40}/>
                 <ProgressBar bsStyle="info" now={20}/>
 
                 <ProgressBar bsStyle="warning" now={60}/>
                 <ProgressBar bsStyle="danger" now={80}/>
+
+                <div>
+                    <div className="row">
+                        <div className="col-xs-3">
+                            ONE
+                        </div>
+                        <div className="col-xs-9">
+                            TWO
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- client/components/firebaseKeys.js
+++ client/components/firebaseKeys.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
     apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  storageBucket: "<BUCKET>.appspot.com"
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com"
 };
 var fire = firebase.initializeApp(config);
 export default fire;
