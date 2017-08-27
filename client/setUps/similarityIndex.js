0fd99a95fc49390b1531a12ca51fa471ba10be34
jyotish09 <dontsentmemails@yahoo.com>
2017-08-27 17:30:08 +0530

    Similarity Indexes Calculation

--- client/setUps/similarityIndex.js
+++ client/setUps/similarityIndex.js
@@ -2,26 +2,43 @@ var data = '',
     promises = [],
     userInterests = data[2],
     movieInterests = data[0],
-    users = data [3];
+    users = data[3];
 
-function nthUser(n){
-    var i = 1 ;
-    for(j in users){
-        if(i == n){
+function nthUser(n) {
+    var i = 1;
+    for (j in users) {
+        if (i == n) {
             return j;
-        }else{i++;}
+        } else {
+            i++;
+        }
     }
 }
 
 for (i in userInterests) {
-    var movieList = _.flatten([userInterests[i].disliked, userInterests[i].liked]), similarity;
+    var movieList = _.flatten([userInterests[i].disliked, userInterests[i].liked]),
+        aLiked = userInterests[i].liked,
+        aDisliked = userInterests[i].disliked,
+        similarity;
     console.log(i);
     var linkedUsers = [];
     for (j in movieList) {
-        linkedUsers = _.flatten(movieInterests[movieList[j]].liked,movieInterests[movieList[j]].disliked);
+        linkedUsers = _.flatten(movieInterests[movieList[j]].liked, movieInterests[movieList[j]].disliked,linkedUsers);
     }
     console.log("linkedUsers");
     console.log(linkedUsers);
+    for (k in linkedUsers) {
+        userDetails = userInterests[nthUser(k)];
+        if (typeof userDetails != 'undefined' && nthUser(k) != i) {
+            console.log(nthUser(k));
+            //console.log(userDetails);
+            var bLiked = userDetails.liked,
+                bDisliked = userDetails.disliked;
+            similarity = (_.intersection(aLiked, bLiked).length+_.intersection(aDisliked, bDisliked).length-_.intersection(aLiked, bDisliked).length-_.intersection(aDisliked, bLiked).length) / _.union(aLiked, bLiked, aDisliked, bDisliked).length
+            console.log("Similarity : "+similarity);
+        }
+
+    }
     // userInterests[nthUser(6)]
-    linkedUsers = [];
+    linkedUsers = [], similarity = 0;
 }
