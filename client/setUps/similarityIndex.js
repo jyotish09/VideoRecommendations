b54e13b8aaef2ffa1db132ff7c982a1c19dea0d6
jyotish09 <dontsentmemails@yahoo.com>
2017-08-27 22:53:05 +0530

    Pushing Similar Users

--- client/setUps/similarityIndex.js
+++ client/setUps/similarityIndex.js
@@ -19,26 +19,30 @@ for (i in userInterests) {
     var movieList = _.flatten([userInterests[i].disliked, userInterests[i].liked]),
         aLiked = userInterests[i].liked,
         aDisliked = userInterests[i].disliked,
-        similarity;
+        similarity,
+        similarUsers = [];
     console.log(i);
     var linkedUsers = [];
     for (j in movieList) {
         linkedUsers = _.flatten(movieInterests[movieList[j]].liked, movieInterests[movieList[j]].disliked,linkedUsers);
     }
-    console.log("linkedUsers");
-    console.log(linkedUsers);
     for (k in linkedUsers) {
         userDetails = userInterests[nthUser(k)];
         if (typeof userDetails != 'undefined' && nthUser(k) != i) {
-            console.log(nthUser(k));
-            //console.log(userDetails);
             var bLiked = userDetails.liked,
                 bDisliked = userDetails.disliked;
             similarity = (_.intersection(aLiked, bLiked).length+_.intersection(aDisliked, bDisliked).length-_.intersection(aLiked, bDisliked).length-_.intersection(aDisliked, bLiked).length) / _.union(aLiked, bLiked, aDisliked, bDisliked).length
-            console.log("Similarity : "+similarity);
+            similarUsers.push({id : nthUser(k), value : similarity});
         }
 
     }
-    // userInterests[nthUser(6)]
-    linkedUsers = [], similarity = 0;
+    similarUsers.sort(function (a, b) {
+      return b.value - a.value;
+    });
+    console.log(similarUsers);
+    firebase.database().ref('similarUsers/' + i).set({
+          similarityIndexes:similarUsers
+        });
+    tempLike =[],tempDislike=[];
+    linkedUsers = [], similarity = 0, similarUsers = [];
 }
